d97736d899745206cd9073cddc61e9ad04c5bae4
Anders Egerup Halager <aeh@cs.au.dk>
2015-02-13 13:39:32 +0100

    tree.c: remove commented out code

--- tree.c
+++ tree.c
@@ -19,14 +19,12 @@ extern double exprate;
 SEQUENCE *rootTime;       /* Base of timeline */
 static SEQUENCE *lastTime; /* Timeline */
 
-/*  static int edgeCounter = 0; */
 extern double selection_rate;
 
 bool probCoalescens(void)
 {
   double sz;
 
-  //if (RZ) return 1;
   sz = (double)size;
   sz = (sz*(sz-1.0))/(sz*(sz-1.0)+sz*selection_rate);
   return (drand48()<sz);
@@ -71,12 +69,6 @@ void makeCoalescensNode(void)
     lastTime = s;
   }
 
-/*    printf("\n\n"); */
-/*    prettyInterval(s1->intervals); */
-/*    printf("\n"); */
-/*    prettyInterval(s2->intervals); */
-/*    printf("\n\n"); */
-
   s->son = s1;
   s->sonID = edgeCounter++;
   s->daughter = s2;
@@ -92,14 +84,6 @@ void makeCoalescensNode(void)
 
   s->matleft = updateOneK();  
 
-/*    printf("N %i|1|%f|",s->ID,s->Time); */
-/*    printf("Time:%f",s->Time); */
-/*    printf("\n"); */
-
-/*    printf("IN %i|",s->ID); */
-/*    prettyInterval(s->intervals); */
-/*    printf("|"); */
-
   if (s->matleft>=0.0) {
 
     if (last_make!=NULL) {
@@ -111,35 +95,11 @@ void makeCoalescensNode(void)
       prettyInterval(i);
     }
   }
-
-/*    printf("\n"); */
-
-/*    if (s->son->indegree == 1) { */
-/*      printf("E %d|%d|%d\n",edgeCounter,s->son->son->ID,s->ID); */
-/*      printf("IE %d|",edgeCounter); */
-/*      prettyInterval(s->son->intervals); */
-/*      printf("\n"); */
-/*      edgeCounter++; */
-/*    } */
-/*    else */
-/*      printf("E %d|%d|%d\n",edgeCounter++,s->son->ID,s->ID); */
-
-/*    if (s->daughter->indegree == 1) { */
-/*      printf("E %d|%d|%d\n",edgeCounter,s->daughter->son->ID,s->ID); */
-/*      printf("IE %d|",edgeCounter); */
-/*      prettyInterval(s->daughter->intervals); */
-/*      printf("\n"); */
-/*      edgeCounter++; */
-/*    } */
-/*    else */
-/*      printf("E %d|%d|%d\n",edgeCounter++,s->daughter->ID,s->ID); */
 }
 
 void makeSelectionNode(void)
 {
   SEQUENCE *s,*r,*s1,*s2;
-/*    double P; */
-/*    bool goodP; */
 
   s = getSomeSequence();
   s->outdegree = 1;
@@ -187,33 +147,7 @@ void makeSelectionNode(void)
   putSequence(s2);
 
   r->sonID = edgeCounter++;
-
-/*    printf("N %i|2|%f|",r->ID,r->Time); */
-/*    printf("Time: %f",r->Time); */
-/*    //printf("P: %f",r->P); */
-/*    printf("\n"); */
-  
-/*    printf("IN %i|%f#",r->ID,r->P); */
-/*    prettyInterval(r->intervals); */
-/*    printf("\n"); */
-
-/*    if (r->son->indegree == 1) { */
-/*      printf("E %i|%i|%i\n",edgeCounter,r->son->son->ID,r->ID); */
-/*      printf("IE %i|",edgeCounter); */
-/*      prettyInterval(r->son->intervals); */
-/*      printf("\n"); */
-/*      edgeCounter++; */
-/*    } */
-/*    else */
-/*      printf("E %i|%i|%i\n",edgeCounter++,r->son->ID,r->ID); */
-
 }
-
-
-/*  static void addTopPoints(SEQUENCE *s) */
-/*  { */
-/*    addRootPoint(s->intervals,s); */
-/*  } */
 
 void build(void)
 {
@@ -224,7 +158,6 @@ void build(void)
   newTime = 0.0;
   initTerminator();
   initSequencePool();
-/*    initRootPoints(); */
 
   for (i=0; i<num_ini_seq; i++) {
     s = newSequence();
@@ -245,18 +178,10 @@ void build(void)
       lastTime->nextTime = s;
       lastTime = s;
     }
-
-/*      printf("N %i|0|%f|",s->ID,s->Time); */
-/*      printf("Time:%f\n",s->Time); */
-/*      printf("IN %d|",s->ID); */
-/*      prettyInterval(s->intervals); */
-/*      printf("|\n"); */
   }
 
   i = num_ini_seq;
   while (size>1) {
-/*      if (size==1) */
-/*        printf("One size!!\n"); */
 
     if (RZ) {
       newTime = newTime + exponen((double)size*((double)size-1.0)/2.0+
@@ -265,9 +190,6 @@ void build(void)
       fprintf(stderr,"Rho must be zero!!\n");
       exit(1);
     }
-
-    //fprintf(stderr,"%f\n",newTime);
-
 
     if (probCoalescens()) {
       makeCoalescensNode();
@@ -278,9 +200,6 @@ void build(void)
       i++;
     }
   }
-
-/*    traverseTopSeqs(addTopPoints); */
-
 }
 
 
@@ -299,7 +218,3 @@ void intersectAll(INTERVAL *i)
   traverseTopSeqs(intersectOne);
   recalculateAllA();
 }
-
-
-
-
